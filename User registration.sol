cca0ab9a11e8966ffa541742da75b68bb05c8813
arunkotla <arunkkotla1998@gmail.com>
2023-05-04 14:52:38 +0530

    first commit

---
+++ User registration.sol
@@ -0,0 +1,29 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract Register {
+
+    struct User {
+        string name;
+        string email;
+        bool isRegistered;
+    }
+
+    mapping(address => User) public users;
+    mapping (string => address) public verification;
+
+    function register_here(string memory _name, string memory _email) public {
+        
+        require(!users[msg.sender].isRegistered, "You have already registered");
+        require(verification[_email] == address(0), "email is already in use");
+
+        users[msg.sender] = User(_name, _email, true);
+        verification[_email] = msg.sender;
+    }
+
+    function authentication_check (string memory _email) public view returns(bool){
+    if (verification[_email] == msg.sender) {
+        return true;}
+        return false;
+    }
+}
